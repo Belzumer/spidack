caf41ff789fa0ce6c01d0ccb1392e042cbcbae95
jpcuencas <juan.pedro.cuenca.sevilla@gmail.com>
2018-06-10 22:33:50 +0200

    ajustes

--- WpfSipiderApp/WpfSipiderApp/App.xaml.cs
+++ WpfSipiderApp/WpfSipiderApp/App.xaml.cs
@@ -11,7 +11,16 @@ namespace WpfSipiderApp
     /// <summary>
     /// Lógica de interacción para App.xaml
     /// </summary>
+    /// <seealso cref="System.Windows.Application" />
     public partial class App : Application
     {
+        /// <summary>
+        /// Ejemploes the specified i.
+        /// </summary>
+        /// <param name="i">The i.</param>
+        void ejemplo(Int32 i)
+        {
+
+        }
     }
 }
